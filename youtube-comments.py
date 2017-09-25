4eec75e98fbe33f09006cb8e5b790a46b902fdba
Jonathan Adalin <jonathan.adalin@mail.mcgill.ca>
2017-09-24 23:42:34 -0400

    Added reply parsing

--- youtube-comments.py
+++ youtube-comments.py
@@ -6,14 +6,26 @@ import json
 # Helper functions
 
 def parse_comment_json(json_file, json_location):
-    with open(os.path.join(json_location, json_file[0])) as json_file:
+    global conversationCount
+    with open(os.path.join(json_location, json_file)) as json_file:
         comment_list = json.loads(json_file.read())
         for comment in comment_list:
-            print comment['id']
+            if comment['hasReplies'] == True:
+                # print 'PARENT ID ' + comment['user']
+                # print 'PARENT CONTENT ' + comment['commentText']
+                for reply in comment['replies']:
+                    # Ignore recursive comments
+                    if '\n' not in reply['commentText']:
+                        # print '    CHILD ID ' + reply['user']
+                        # print '    CHILD CONTENT ' + reply['commentText']
+                        conversationCount += 1
 
 # Main
 
+conversationCount = 0
 json_location = 'json/'
-json_files = [os.listdir(json_location)]
+json_files = os.listdir(json_location)
 for json_file in json_files:
     parse_comment_json(json_file, json_location)
+
+print conversationCount
